e798358caa2339f092801ecff8575d2bc0730c07
deepak-03 <42989439+deepak-03@users.noreply.github.com>
2021-03-08 18:03:58 +0530

    Add files via upload

---
+++ task_1.js
@@ -0,0 +1,39 @@
+const img_arr = [
+  {
+    photo: "C:\Users\hp\Downloads\task_101\imgs\img_1.jpg"
+  },
+  {
+    photo:"C:\Users\hp\Downloads\task_101\imgs\img_2.jpg"
+  },
+  {
+    photo:"C:\Users\hp\Downloads\task_101\imgs\img_3.jpg"
+  },
+  {
+    photo:"C:\Users\hp\Downloads\task_101\imgs\img_4.jpg"
+  },
+  {
+    photo:"C:\Users\hp\Downloads\task_101\imgs\img_5.jpg"
+  },
+  {
+    photo:"C:\Users\hp\Downloads\task_101\imgs\img_6.jpg"
+  },
+  {
+    photo:"C:\Users\hp\Downloads\task_101\imgs\img_7.jpg"
+  },
+  {
+    photo:"C:\Users\hp\Downloads\task_101\imgs\img_8.jpg"
+  },
+  {
+    photo:"C:\Users\hp\Downloads\task_101\imgs\img_9.jpg"
+  },
+  {
+    photo:"C:\Users\hp\Downloads\task_101\imgs\img_10.jpg"
+  },
+  {
+    photo:"C:\Users\hp\Downloads\task_101\imgs\img_11.jpg"
+  }
+];
+
+document.getElementById("app").innerHTML = `
+<p>Hello World</p>
+`
