fd262e73d6f8a7599a0959b561c0b9d96da02ba5
deepak-03 <deepakagarwal03.da@gmail.com>
2021-03-15 11:07:02 +0530

    implemented tabs

--- task_1.js
+++ task_1.js
@@ -1,14 +1,12 @@
-
-
 fetch("./task_1.json")
   .then(resp => resp.json())
   .then(buildPage);
 
 // FOLLOW BUTTON
 
-let followed = false;
-
+(function follow(){
 let followButton = document.getElementById("followButton");
+let followed = false;
   followButton.onclick = () => {
   let followCountDiv = document.getElementById("numberOfFollowers");
   followButton.classList.toggle("followed");
@@ -16,6 +14,7 @@ let followButton = document.getElementById("followButton");
   followCountDiv.innerText = parseInt(followCountDiv.innerText) + (followed?-1:1);
   followed = !followed;
 }
+})()
 
 // ACCOUNT PIC
 
@@ -109,44 +108,127 @@ function buildPosts(postsArray) {
  // LIKE ICON TOGGLING
 
   let liked = false;
-    let likeI = document.getElementById("likeIcon");
+    // let likeI = document.getElementById("likeIcon");
     likeButton.onclick = () => {
     // console.log(5);
     // let likeCountDiv = document.getElementById("numberOfLikes");
     // console.log(5);
     likeIcon.classList.toggle("UnLikedIcon");
-    // console.log(likeIcon.classList);
+    console.log(likeIcon.classList);
     // likeCountDiv.innerText = parseInt(likeCountDiv.innerText) + (liked?-1:1);
     // liked = !liked;
-    
-                                }
+    console.log(likeButton.innerHTML);
+                              }
   })
 }  
 
-// IMPLEMENTING 2 TABS
+//POSTING VIDEOS
+function buildVideos(postsArray) {
+  const videoContainer = document.getElementById("igtvContainer");
+  postsArray.forEach(({id, vidLink, likes, comments}) => {
 
-function onTabClick(event){
-  let activeTabs = document.querySelectorAll(".active");
+    const vidContainer = document.createElement("div");
+    vidContainer.classList.add("imagePost");
 
-  // console.log(activeTabs.length);
+    const vid = document.createElement("video");
+    vid.src = vidLink;
+    vid.controls = true;
+    vid.classList.add("images");
+    
+    // Creating a div container for buttons(icons and numbers)
+    const likeCommentDiv = document.createElement("div");
+    likeCommentDiv.classList.add("likeComment");
 
-  for(var i=0; i<activeTabs.length; i++){
-    activeTabs[i].className =  activeTabs[i].className.replace("active","");
-  }
+    const likeButton = document.createElement("button");
+    likeButton.classList.add("likeCommentButton");
+    const commentButton = document.createElement("button");
+    commentButton.classList.add("likeCommentButton");
 
-  event.target.parentElement.className += " active";
-  // console.log(document.getElementById(event.target.href.split('#')[1]).className);  
-  document.getElementById(event.target.href.split('#')[1]).className += ' active';
+    const likeIcon = document.createElement("i");
+    const commentIcon = document.createElement("i");
+
+    likeIcon.classList.add("fas");
+    likeIcon.classList.add("fa-heart");
+    likeIcon.classList.add("onHoverIcon");
+    commentIcon.classList.add("fas");
+    commentIcon.classList.add("fa-comment");
+    commentIcon.classList.add("onHoverIcon");
+
+    likeIcon.id = "ICON" + id;
+    // console.log(likeIcon.id);
+
+    const numberOfLikes = document.createElement("div");
+    const numberOfComments = document.createElement("div");
+
+    numberOfLikes.innerText = likes;
+    numberOfLikes.id = "LIKES" + id;
+    // console.log(numberOfLikes.id);
+    numberOfComments.innerText = comments;
+
+    likeButton.appendChild(likeIcon);
+    likeButton.appendChild(numberOfLikes);
+
+    commentButton.appendChild(commentIcon);
+    commentButton.appendChild(numberOfComments);
+
+    likeCommentDiv.appendChild(likeButton);
+    likeCommentDiv.appendChild(commentButton);
+
+    vidContainer.appendChild(vid);
+    vidContainer.appendChild(likeCommentDiv);
+
+    videoContainer.appendChild(vidContainer);
+
+
+  })
 }
 
-const ele = document.getElementById("menuContainer");
 
-ele.addEventListener('click', onTabClick, false);
+// IMPLEMENTING 2 TABS
+// function onTabClick(event){
+//   let activeTabs = document.querySelectorAll(".active");
 
+  // console.log(activeTabs);
+  // console.log(event.target);
+
+  // for(var i=0; i<activeTabs.length; i++){
+  //   activeTabs[i].className =  activeTabs[i].className.replace("active","");
+  // }
+
+  // event.target.parentElement.className += " active";
+  // console.log(event.target.parentElement);
+  // console.log(document.getElementById(event.target.href.split('#')[1]).className);  
+  // document.getElementById(event.target.href.split('#')[1]).className += ' active';
+// }
+
+// const ele = document.getElementById("menuContainer");
+
+// ele.addEventListener('click', onTabClick, false);
+
+
+const postLink = document.getElementById("imageLink");
+const igtvLink = document.getElementById("igtvLink");
+const post = document.getElementById("postsHere");
+const igtv = document.getElementById("igtvHere");
+
+igtvLink.onclick = () =>{
+  igtv.classList.remove("hidden");
+  postLink.classList.remove("active");
+  post.classList.add("hidden");
+  igtvLink.classList.add("active");
+}
+
+postLink.onclick = () =>{
+  post.classList.remove("hidden");
+  igtvLink.classList.remove("active");
+  igtv.classList.add("hidden");
+  postLink.classList.add("active");
+}
 
 function buildPage({accountPic, profilePic, profileData, igtv, posts}) {
   buildProfilePic(profilePic);
   buidProfileData(profileData);
   buildAccountPic(accountPic);
   buildPosts(posts);
+  buildVideos(igtv);
 }
